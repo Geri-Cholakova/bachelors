a962e8cf05c1755be12facd233f2fafbcae2068d
Gergana Cholakova <G.Cholakova@campus.lmu.de>
2025-05-07 10:15:09 +0200

    With a dust grain instead of planatesimal as the third body

--- bachelors/rebound-2body_collision.py
+++ bachelors/rebound-2body_collision.py
@@ -5,14 +5,14 @@ def setupSimulation():
     sim.integrator = "ias15" # IAS15 is the default integrator, so we don't need this line
     sim.collision = "line" #this scales as o(n^2), use a diff one for a bigger simulation
     #sim.collision_resolve = "merge" #just for example
-    sim.add(m=1.,r=0.05 ) #for m in solar masses, a,r in AU 
-    sim.add(m=1e-3,r=0.005, a=1.)
-    sim.add(m=3e-6,r=0.0005, a=1.1)
-    sim.move_to_com()
+    sim.add(m=1.,r=4.67e-3 )  #parameters of the Sun
+    sim.add(m=3e-6,r=4.26e-5, a=1) #parameters of Earth
+    sim.add(m=3e-32, r=1e-11, a=1.02) #example dust stone w radius several km
     return sim
 sim = setupSimulation()
 t=float(input("iterations of the simulation? (local years): ")) #enter how many years
-sim.integrate(t*2.*np.pi) #we have collision between 200 and 250 iterations 
+sim.integrate(t*2.*np.pi) #we have collision between 925 and 950 iterations
+sim.move_to_com()
 for o in sim.orbits():
     print(o)
     rebound.OrbitPlot(sim)
