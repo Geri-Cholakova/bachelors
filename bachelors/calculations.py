8c79f42c923e4bdb7b106f4963bcbb1006bffc3e
Gergana Cholakova <G.Cholakova@campus.lmu.de>
2025-06-11 14:34:03 +0200

    Created tests to compare theory v_r = St * eta * v_k and v_z = St * z * Omega. Corrected code so that it works in cart. coord for simplicity

--- bachelors/calculations.py
+++ bachelors/calculations.py
@@ -12,10 +12,10 @@ from astropy import units as u
 norm = float(np.sqrt(const.G.value * const.M_sun.value / const.au.value))
 r_H = 0.01
 
-def cs_rho(x, y, z=0, r_h=r_H):
-    x = (x * r_h + 1) * const.au.value
-    y = y * r_h * const.au.value
-    z = z * r_h * const.au.value  # here in AU's
+def cs_rho(x=1.0, y=0, z=0):
+    x = x * const.au.value
+    y = y * const.au.value
+    z = z * const.au.value  # here in AU's
     r = np.sqrt(x ** 2 + y ** 2)
     T = 200 * np.sqrt(const.au.value / r)
     Sigma = 2000 * (const.au.value / r)
@@ -28,7 +28,7 @@ def cs_rho(x, y, z=0, r_h=r_H):
     return (c_s, rho)
 
 
-def velocities(s, x, y, r_h=r_H, n=-2.75, z=0, eps=0.01):
+def velocities_rh(s, x, y, r_h=r_H, n=-2.75, z=0, eps=0.01):
     # everything should be in si
     x = (x * r_h + 1) * const.au.value
     y = y * r_h * const.au.value
@@ -73,7 +73,55 @@ def velocities(s, x, y, r_h=r_H, n=-2.75, z=0, eps=0.01):
         v_dust_x / norm,
         v_dust_y / norm,
         v_gas_x / norm,
-        v_gas_y / norm
+        v_gas_y / norm,
+    )
+
+def velocities_cart(s, x, y, n=-2.75, z=0, eps=0.01):
+    # everything should be in si
+    x = x * const.au.value
+    y = y * const.au.value
+    z = z * const.au.value  # here in AU's
+    rho_s = 1500
+    R = np.sqrt(x ** 2 + y ** 2)
+    T = 200 * np.sqrt(const.au.value / R)
+    Sigma = 2000 * (const.au.value / R)
+    mu = 2.3
+    c_s = np.sqrt(const.k_B.value * T / (mu * const.m_p.value))
+    Omega = np.sqrt(const.G.value * const.M_sun.value / R ** 3)
+    h = c_s / Omega
+    exponent = -(z ** 2) / (2 * h)
+    rho = (Sigma / (np.sqrt(2 * np.pi) * h)) * np.e ** exponent
+    t_fric = rho_s * s / (rho * c_s * np.sqrt(8 / np.pi))
+    St = t_fric * Omega
+    # we assume homogenous gas disk with P prop. to r**n
+    # Omega prop r**-1.5, c_s prop r**-0.25, Sigma prop r**-1, P prop Omega*Sigma*c_s
+    # So n = dlnP/dlnr = -2.75
+    nu = -0.5 * (h / R) ** 2 * n
+    # we use units for m=1, g=1, so we need to norm the result
+    v_k = Omega * R
+    v_dust_r = -2 / (St + (1 + eps) ** 2 / St) * nu * v_k
+    v_dust_phi = v_k - (1 + eps) / ((1 + eps) ** 2 + St ** 2) * nu * v_k
+    v_gas_r = -2 * eps / (St + (1 + eps) ** 2 / St) * nu * v_k
+    v_gas_phi = (
+        v_k + (eps / ((1 + eps) * (1 + St ** 2 / (1 + eps) ** 2)) - 1) * nu * v_k
+    )
+
+    sina = y / R
+    cosa = x / R
+
+    v_kep_x = -v_k * sina
+    v_kep_y = v_k * cosa
+    v_dust_x = v_dust_r * cosa - v_dust_phi * sina
+    v_dust_y = v_dust_r * sina + v_dust_phi * cosa
+    v_gas_x = v_gas_r * cosa - v_gas_phi * sina
+    v_gas_y = v_gas_r * sina + v_gas_phi * cosa
+    return (
+        St,
+        nu,
+        v_dust_x / norm,
+        v_dust_y / norm,
+        v_gas_x / norm,
+        v_gas_y / norm,
     )
 
 
--- bachelors/force_example.py
+++ bachelors/force_example.py
@@ -11,13 +11,13 @@ from astropy import constants as const
 from astropy import units as u
 import calculations as calc
 
-r_pebble = 0.7958
-dist_pl = 1
+r_pebble = 1
+dist_pl = 1.05
 M_star = 1
 M_planet = 3e-6
-inclin = 0
+inclin = 1e-7
 
-if inclin > 4e-6:
+if inclin > 1e-5:
     print("Your z is too large, there's a risk rho = 0")
 else:
     
@@ -36,7 +36,7 @@ else:
     sim.G = 1
     sim.add(m=M_star, r=4.67e-3)
     #sim.add(m=M_planet, r=4.26e-5, a=dist_pl)
-    sim.add(m=m_pebble*norm_kg, r=r_pebble*norm_m, a=dist_pl * 1.05, inc=inclin)
+    sim.add(m=m_pebble*norm_kg, r=r_pebble*norm_m, x=dist_pl * 1., y=0, z=inclin*dist_pl)
     sim.move_to_com()
     ps = sim.particles
     # defining a non-conservative force F = m*v/tau
@@ -51,7 +51,7 @@ else:
         pwr = 0
         A = 0.44
     C_D = A * Re ** pwr
-    
+    coef_S = C_D * np.pi * (r_pebble) ** 2 * np.sqrt(8/np.pi) /norm_s / m_pebble
     # def stokes for x, y only
     
     def Stokesdrag(reb_sim):
@@ -59,38 +59,15 @@ else:
         py = ps[1].y
         pz = ps[1].z
     
-        c_s, rho = calc.T_rho(x=(px - 1) / r_H, y=py / r_H, z=pz / r_H)
-        v_kep_x, v_kep_y, v_dust_x, v_dust_y, v_gas_x, v_gas_y = calc.velocities(
-            s=r_pebble, x=(px - 1) / r_H, y=py / r_H, z=pz / r_H
+        c_s, rho = calc.cs_rho(x=px, y=py, z=pz)
+        St, nu, v_dust_x, v_dust_y, v_gas_x, v_gas_y = calc.velocities_cart(
+            s=r_pebble, x=px, y=py, z=pz
         )
     
         
-        ps[1].ax -= (
-            C_D
-            * np.pi
-            * (r_pebble) ** 2
-            * rho 
-            * (ps[1].vx - v_gas_x)
-            * c_s * np.sqrt(8/np.pi) /norm_s
-            / m_pebble
-        )
-        ps[1].ay -= (
-            C_D
-            * np.pi
-            * (r_pebble) ** 2
-            * rho
-            * (ps[1].vy - v_gas_y)
-            * c_s * np.sqrt(8/np.pi) / norm_s
-            / m_pebble
-        )
-        ps[1].az -= (
-            C_D
-            * np.pi
-            * (r_pebble) ** 2
-            * rho
-            * ps[1].vz / norm_s
-            * c_s * np.sqrt(8/np.pi)/ m_pebble
-        )
+        ps[1].ax -= coef_S * rho * (ps[1].vx - v_gas_x) * c_s 
+        ps[1].ay -= coef_S * rho* (ps[1].vy - v_gas_y)* c_s
+        ps[1].az -= coef_S * rho * ps[1].vz  * c_s 
     
     coef_E = np.sqrt(8 / np.pi)/ (1500 * r_pebble)/ norm_s
     # we're norming that for rho_dust, 
@@ -102,9 +79,9 @@ else:
         pz = ps[1].z
     
     
-        c_s, rho = calc.cs_rho(x=(px - 1) / r_H, y=py / r_H, z=pz / r_H)
-        v_kep_x, v_kep_y, v_dust_x, v_dust_y, v_gas_x, v_gas_y = calc.velocities(
-            s = r_pebble, x = (px - 1)/r_H, y= py/r_H, z= pz/r_H)
+        c_s, rho = calc.cs_rho(x=px, y=py, z=pz)
+        St, nu, v_dust_x, v_dust_y, v_gas_x, v_gas_y = calc.velocities_cart(
+            s = r_pebble, x = px, y= py, z= pz)
         # everything thats not in those patameters is incl in coef_E
     
         ps[1].ax -= coef_E * c_s *rho * (ps[1].vx - v_gas_x)
@@ -114,18 +91,47 @@ else:
         #i wanna break here
     
     sim.additional_forces = Epsteindrag
-    sim.force_is_velocity_dependent = 0
+    sim.force_is_velocity_dependent = 1
     
-    Nout = 5000
+    Nout = 1000
+    comparison = np.zeros((6, Nout))
     r = np.zeros(Nout)
-    times = np.linspace(0.0, 100 * 2.0 * np.pi, Nout)
+    z = np.zeros(Nout)
+    v_th = np.zeros(Nout)
+    vz_th = np.zeros(Nout)
+    v_code = np.zeros(Nout-1)
+    vz_code = np.zeros(Nout-1)
+    St = np.zeros(Nout)
+    nu = np.zeros(Nout)
+    z_th = np.zeros(Nout)
+    times = np.linspace(0.0, 10 * 2.0 * np.pi, Nout)
     for i, time in enumerate(times):
         sim.integrate(time, exact_finish_time=0 )
         if not np.isfinite(sim.particles[1].x):
            print(f"NaN encountered at t = {time}", ps[1].vx, ps[1].vy, ps[1].vz)
            break
-        r[i] = np.hypot(sim.particles[1].x, sim.particles[1].y)
+        comparison[:, i] = calc.velocities_cart(s = r_pebble, x = ps[1].x, y = ps[1].y,
+                                          z = ps[1].z)
+        St[i] = comparison[0, i]
+        nu[i] = comparison[1, i]  
+        r[i] = np.hypot(ps[1].x, ps[1].y)* const.au.value #in m
+        z[i] = ps[1].z* const.au.value #in m
+        v_th[i] = -St[i] * nu[i] * np.sqrt(const.G.value * const.M_sun.value/r[i]) #in m/s
+        vz_th[i] = -St[i] * z[i] * np.sqrt(const.G.value * const.M_sun.value/r[i]**3) #in m/s
+    
+    for i in range(Nout-1):
+        v_code[i] = (r[i+1]-r[i])/(times[1]/norm_s) #in m/s
+        vz_code[i] = (z[i+1]-z[i])/(times[1]/norm_s) #in m/s
     rebound.OrbitPlot(sim)
     fig = plt.figure(figsize=(15,5))
     ax = plt.subplot(111)
-    plt.plot(times, r);
+    plt.plot(times[1:]/(2*np.pi), v_code,'r',times[1:]/(2*np.pi), v_th[1:],'g-');
+    fig = plt.figure(figsize=(15,5))
+    ax = plt.subplot(111)
+    plt.plot(times[1:]/(2*np.pi), vz_code,'r',times[1:]/(2*np.pi), vz_th[1:],'g-');
+    fig = plt.figure(figsize=(15,5))
+    ax = plt.subplot(111)
+    plt.plot(times/(2*np.pi), r/const.au.value);
+    fig = plt.figure(figsize=(15,5))
+    ax = plt.subplot(111)
+    plt.plot(times/(2*np.pi), z);
