Calculations.py now is correct after a flip in a sign in v_dust. force_example functions as well

bachelors/calculations.py
@@ -8,6 +8,7 @@ import numpy as np
 import math
 from astropy import constants as const
 from astropy import units as u
+import matplotlib.pyplot as plt
 
 norm = float(np.sqrt(const.G.value * const.M_sun.value / const.au.value))
 r_H = 0.01
@@ -23,7 +24,7 @@ def cs_rho(x=1.0, y=0, z=0):
     c_s = np.sqrt(const.k_B.value * T / (mu * const.m_p.value))
     Omega = np.sqrt(const.G.value * const.M_sun.value / r ** 3)
     h = c_s / Omega
-    exponent = -(z ** 2) / (2 * h)
+    exponent = -(z ** 2) / (2 * h**2)
     rho = (Sigma / (np.sqrt(2 * np.pi) * h)) * np.e ** exponent
     return (c_s, rho)
 
@@ -41,7 +42,7 @@ def velocities_rh(s, x, y, r_h=r_H, n=-2.75, z=0, eps=0.01):
     c_s = np.sqrt(const.k_B.value * T / (mu * const.m_p.value))
     Omega = np.sqrt(const.G.value * const.M_sun.value / R ** 3)
     h = c_s / Omega
-    exponent = -(z ** 2) / (2 * h)
+    exponent = -(z ** 2) / (2 * h**2)
     rho = (Sigma / (np.sqrt(2 * np.pi) * h)) * np.e ** exponent
     t_fric = rho_s * s / (rho * c_s * np.sqrt(8 / np.pi))
     St = t_fric * Omega
@@ -76,22 +77,21 @@ def velocities_rh(s, x, y, r_h=r_H, n=-2.75, z=0, eps=0.01):
         v_gas_y / norm,
     )
 
-def velocities_cart(s, x, y, n=-2.75, z=0, eps=0.01):
-    # everything should be in si
+def velocities_cart(s, x, y, n=-2.75, z=0, eps=0.01, rho_s = 1500):
+    # coord should be in rebound units
     x = x * const.au.value
     y = y * const.au.value
     z = z * const.au.value  # here in AU's
-    rho_s = 1500
-    R = np.sqrt(x ** 2 + y ** 2)
+    R = np.sqrt(x ** 2 + y ** 2) #answer in si
     T = 200 * np.sqrt(const.au.value / R)
     Sigma = 2000 * (const.au.value / R)
     mu = 2.3
     c_s = np.sqrt(const.k_B.value * T / (mu * const.m_p.value))
     Omega = np.sqrt(const.G.value * const.M_sun.value / R ** 3)
     h = c_s / Omega
-    exponent = -(z ** 2) / (2 * h)
-    rho = (Sigma / (np.sqrt(2 * np.pi) * h)) * np.e ** exponent
-    t_fric = rho_s * s / (rho * c_s * np.sqrt(8 / np.pi))
+    exponent = -(z ** 2) / (2 * h**2)
+    rho = (Sigma / (np.sqrt(2 * np.pi) * h)) * np.e**exponent
+    t_fric = rho_s * s / (rho * c_s) * np.sqrt( np.pi/ 8)
     St = t_fric * Omega
     # we assume homogenous gas disk with P prop. to r**n
     # Omega prop r**-1.5, c_s prop r**-0.25, Sigma prop r**-1, P prop Omega*Sigma*c_s
@@ -99,12 +99,14 @@ def velocities_cart(s, x, y, n=-2.75, z=0, eps=0.01):
     nu = -0.5 * (h / R) ** 2 * n
     # we use units for m=1, g=1, so we need to norm the result
     v_k = Omega * R
-    v_dust_r = -2 / (St + (1 + eps) ** 2 / St) * nu * v_k
-    v_dust_phi = v_k - (1 + eps) / ((1 + eps) ** 2 + St ** 2) * nu * v_k
-    v_gas_r = -2 * eps / (St + (1 + eps) ** 2 / St) * nu * v_k
-    v_gas_phi = (
-        v_k + (eps / ((1 + eps) * (1 + St ** 2 / (1 + eps) ** 2)) - 1) * nu * v_k
-    )
+    
+    A = (1 + eps)/((1+eps)**2 + St**2)
+    B = -2/(St + (1+eps)**2/ St)
+    
+    v_dust_r = B * nu * v_k
+    v_dust_phi = v_k - A * nu * v_k
+    v_gas_r = -eps * B * nu * v_k
+    v_gas_phi = v_k + (eps * A - 1) * nu * v_k
 
     sina = y / R
     cosa = x / R
@@ -123,25 +125,26 @@ def velocities_cart(s, x, y, n=-2.75, z=0, eps=0.01):
         v_gas_x / norm,
         v_gas_y / norm,
     )
+"""
+x2 = 1
+y2 = 0
 
+vel1 = velocities_rh(1e-6, x2, y2)          
+vel2 = velocities_cart(s = 1e-6, x=x2, y = y2)  
+cs, rho = cs_rho(x = 2, y = 0.,z=0.)
+print(vel2[0], cs, rho)
 
-"""for xn in range(-900, 1000, 10):
-    for yn in range(-900, 1000, 10):
-        vel = velocities(s=1, x=xn, y=yn, r_h=0.01)
-        if (vel[2] ** 2 + vel[3] ** 2) > (vel[4] ** 2 + vel[5] ** 2):
-            print(1 / (vel[0] ** 2 + vel[1] ** 2))"""
-"""  
 sim = rebound.Simulation()
 sim.add(m=1)
-sim.add(m=3.14e-27, x=1, y=0, vx=0, vy=1)
-sim.add(m=1e-30, x=0, y=1, vx=vel[2], vy=vel[3])
-sim.add(m=1e-30, x=0.7, y=0.7, vx=vel2[4], vy=vel2[5])
+sim.add(m=2.09e-30, x=x1, y=y1, vx=vel[2], vy=vel[3])
+sim.add(m=2.09e-30, x=x2, y=y2, vx=vel2[4], vy=vel2[5])
 sim.move_to_com()
 # to not have a drift because of center of mass movement
-sim.integrate(2.0 * np.pi * 1000)
+sim.integrate(2.0 * np.pi * 1)
 # seeing the simulation after time t=100 for 2*pi*t=1 yr for a=1, M_central=1
-sim.status()
-# seeing the data on which basis the plot is made
 rebound.OrbitPlot(sim)
 # plot the orbital data after t iterations
-""" 
+print(f' after:{sim.particles[1].a}, {sim.particles[2].a}', vel[0], vel2[0])
+print(sim.particles[1].e, sim.particles[2].e, vel2[6], vel2[7])
+
+"""

bachelors/force_example.py
@@ -11,36 +11,51 @@ from astropy import constants as const
 from astropy import units as u
 import calculations as calc
 
-r_pebble = 1
-dist_pl = 1.05
+r_pebble = 1e-2 #here in m
+dist_pl = 1
+#distance is measured in AUs
 M_star = 1
-M_planet = 3e-6
+#mass is measured in solar units
 inclin = 1e-7
+eps = 0.01
+rho_s = 1500
 
-if inclin > 1e-5:
+if inclin > 1e-3:
     print("Your z is too large, there's a risk rho = 0")
 else:
     
     norm_kg = 1/const.M_sun.value
-    norm_m = 1/const.au.value
-    norm_s = 2.0 * np.pi/(1*u.year).si.value
+    norm_m = 1/const.au.value #in rebound unit * m ^-1
+    norm_s = 2.0 * np.pi/(1*u.year).si.value # in rebound unit * s^-1
     
     
-    r_H = dist_pl * np.cbrt(M_planet / (3 * (M_planet + M_star)))
-    m_pebble= 4 * np.pi * r_pebble ** 3 * 1500 / 3 
+    m_pebble= 4 * np.pi * r_pebble ** 3 * rho_s / 3 #in kg
     
+    x = dist_pl * 1.0
+    y = 0.0
+    z = inclin * dist_pl
+    
+    reb_calc = calc.velocities_cart(r_pebble, x, y, z = z)
+    St = reb_calc[0] #unitless
+    v_dust_x = reb_calc[2] #rebound u
+    v_dust_y = reb_calc[3] #rebound u
+    R = np.sqrt(x**2 + y**2) #rebound u
     Re = 2
     
     sim = rebound.Simulation()
     sim.integrator = "whfast"
     sim.G = 1
+    
+    Omega = np.sqrt(sim.G * M_star/R**3) # in rebound u
     sim.add(m=M_star, r=4.67e-3)
-    #sim.add(m=M_planet, r=4.26e-5, a=dist_pl)
-    sim.add(m=m_pebble*norm_kg, r=r_pebble*norm_m, x=dist_pl * 1., y=0, z=inclin*dist_pl)
+    
+    sim.add(m=m_pebble*norm_kg, r=r_pebble*norm_m, x=x, y=y, z=z, vx= v_dust_x, vy = v_dust_y, 
+            vz = -St * z * Omega )
     sim.move_to_com()
     ps = sim.particles
     # defining a non-conservative force F = m*v/tau
     # coeff for Stokes
+    """
     if Re < 1:
         pwr = -1
         A = 24
@@ -67,11 +82,10 @@ else:
         
         ps[1].ax -= coef_S * rho * (ps[1].vx - v_gas_x) * c_s 
         ps[1].ay -= coef_S * rho* (ps[1].vy - v_gas_y)* c_s
-        ps[1].az -= coef_S * rho * ps[1].vz  * c_s 
+        ps[1].az -= coef_S * rho * ps[1].vz  * c_s """
     
-    coef_E = np.sqrt(8 / np.pi)/ (1500 * r_pebble)/ norm_s
+    coef_E = np.sqrt(8 / np.pi) / (rho_s * r_pebble) /norm_s
     # we're norming that for rho_dust, 
-    # we've defined epstein for x, y only!!!!
     
     def Epsteindrag(reb_sim):
         px = ps[1].x
@@ -82,7 +96,7 @@ else:
         c_s, rho = calc.cs_rho(x=px, y=py, z=pz)
         St, nu, v_dust_x, v_dust_y, v_gas_x, v_gas_y = calc.velocities_cart(
             s = r_pebble, x = px, y= py, z= pz)
-        # everything thats not in those patameters is incl in coef_E
+        # everything thats not in those parameters is incl in coef_E
     
         ps[1].ax -= coef_E * c_s *rho * (ps[1].vx - v_gas_x)
         ps[1].ay -= coef_E * c_s *rho * (ps[1].vy - v_gas_y) 
@@ -93,20 +107,25 @@ else:
     sim.additional_forces = Epsteindrag
     sim.force_is_velocity_dependent = 1
     
-    Nout = 1000
+    #sim.integrate(sim.t + 2* np.pi )  # advance slightly so that a=/= 0 for first timestep
+    
+    Nout = 50000
     comparison = np.zeros((6, Nout))
     r = np.zeros(Nout)
     z = np.zeros(Nout)
     v_th = np.zeros(Nout)
     vz_th = np.zeros(Nout)
-    v_code = np.zeros(Nout-1)
-    vz_code = np.zeros(Nout-1)
+    v_code = np.zeros(Nout)    
+    vz_code1 = np.zeros(Nout)
+    v_code1 = np.zeros(Nout)
+    vz_code = np.zeros(Nout)
     St = np.zeros(Nout)
     nu = np.zeros(Nout)
     z_th = np.zeros(Nout)
+    v_gas_r = np.zeros(Nout)
     times = np.linspace(0.0, 10 * 2.0 * np.pi, Nout)
     for i, time in enumerate(times):
-        sim.integrate(time, exact_finish_time=0 )
+        sim.integrate(time, exact_finish_time=1 )
         if not np.isfinite(sim.particles[1].x):
            print(f"NaN encountered at t = {time}", ps[1].vx, ps[1].vy, ps[1].vz)
            break
@@ -114,24 +133,45 @@ else:
                                           z = ps[1].z)
         St[i] = comparison[0, i]
         nu[i] = comparison[1, i]  
-        r[i] = np.hypot(ps[1].x, ps[1].y)* const.au.value #in m
-        z[i] = ps[1].z* const.au.value #in m
-        v_th[i] = -St[i] * nu[i] * np.sqrt(const.G.value * const.M_sun.value/r[i]) #in m/s
-        vz_th[i] = -St[i] * z[i] * np.sqrt(const.G.value * const.M_sun.value/r[i]**3) #in m/s
+        r[i] = np.hypot(ps[1].x, ps[1].y)#in rebound units
+        z[i] = ps[1].z #in rebound units
+        v_th[i] = -2/ (St[i] + (1+ eps)**2/St[i]) * nu[i] * np.sqrt(const.G.value * const.M_sun.value/(r[i]* const.au.value)) #in m/s
+        vz_th[i] = -St[i] * z[i] * const.au.value * np.sqrt(const.G.value * const.M_sun.value/(r[i]* const.au.value)**3) #in m/s
+        # approx only for St<1
+        v_gas_r[i] = (comparison[4, i]* ps[1].x/r[i] + comparison[5, i]* ps[1].y/r[i])/norm_m #in m
+        v_code1[i] = ps[1].vx* ps[1].x/r[i] + ps[1].vy* ps[1].y/r[i] #in rebound u
+        vz_code1[i] = ps[1].vz #in rebound u
+        
+    #a_code = a_code * norm_s**2 / norm_s
+    v_code = v_code1 * const.au.value *norm_s #in m/s
+    vz_code = vz_code1 * const.au.value *norm_s #in m/s
     
-    for i in range(Nout-1):
-        v_code[i] = (r[i+1]-r[i])/(times[1]/norm_s) #in m/s
-        vz_code[i] = (z[i+1]-z[i])/(times[1]/norm_s) #in m/s
-    rebound.OrbitPlot(sim)
+    plt.figure(figsize=(15, 5))
+    plt.plot(times/(2*np.pi), v_code, 'r-', label='v_code')
+    plt.plot(times/(2*np.pi), v_th, 'g--', label='v_th')
+    plt.legend(); plt.show()
+    
+    plt.figure(figsize=(15, 5))
+    plt.plot(times/(2*np.pi), vz_code, 'r-', label='vz_code')
+    plt.plot(times/(2*np.pi), vz_th, 'g--', label='vz_th')
+    #plt.plot(times[:Nout-1]/(2*np.pi), k, 'b-', label='vz_calc_steps')
+    plt.legend(); plt.show()
+
     fig = plt.figure(figsize=(15,5))
     ax = plt.subplot(111)
-    plt.plot(times[1:]/(2*np.pi), v_code,'r',times[1:]/(2*np.pi), v_th[1:],'g-');
+    plt.plot(times/(2*np.pi), r);
+    
     fig = plt.figure(figsize=(15,5))
     ax = plt.subplot(111)
-    plt.plot(times[1:]/(2*np.pi), vz_code,'r',times[1:]/(2*np.pi), vz_th[1:],'g-');
+    plt.plot(times[1:]/(2*np.pi), (v_code[1:]-v_th[1:])/v_th[1:]);
+    
     fig = plt.figure(figsize=(15,5))
     ax = plt.subplot(111)
-    plt.plot(times/(2*np.pi), r/const.au.value);
-    fig = plt.figure(figsize=(15,5))
-    ax = plt.subplot(111)
-    plt.plot(times/(2*np.pi), z);
+    plt.plot(times/(2*np.pi), z * const.au.value);
+    
+    t_stop = St[0] / Omega
+    del_v = np.mean(v_code - v_th)
+    print("Delta x: ", del_v * t_stop)
+
+    
+    